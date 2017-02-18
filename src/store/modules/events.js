e6a510e32524d35dc1ed2fdd135b38c93e17ce1f
Guy Brukhis <guy.br7@gmail.com>
2017-02-18 07:21:26 +0200

    events - add a dictionary to store with the latest event id of each title

--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -2,11 +2,13 @@ import eventsAPI from '../../api/events'
 import * as types from '../mutation-types'
 import Vue from 'vue'
 import _ from 'lodash'
+import moment from 'moment'
 
 const state = {
   version: 0,
   data: [],
-  transactions: []
+  transactions: [],
+  dictionary: {}
 }
 
 const actions = {
@@ -14,6 +16,7 @@ const actions = {
     if (state.transactions.length === 0) {
       eventsAPI.getEvents().then(({ version, data }) => {
         commit(types.SET_EVENTS, { version, data })
+        commit(types.BUILD_DICTIONARY)
       })
       .catch((payload) => {
         console.error(payload)
@@ -30,6 +33,7 @@ const actions = {
       eventsAPI.postEvents(state.version, state.transactions).then(({ version, data }) => {
         if (data) {
           commit(types.SET_EVENTS, { version, data })
+          commit(types.BUILD_DICTIONARY)
         } else {
           commit(types.SET_EVENTS_VERSION, { version })
           commit(types.CLEAR_EVENTS_TRANSACTIONS)
@@ -51,14 +55,17 @@ const actions = {
 
   addEvent({ commit }, event) {
     commit(types.ADD_EVENT, event)
+    commit(types.SET_DICTIONARY_KEY, event)
   },
 
   setEvent({ commit }, event) {
     commit(types.SET_EVENT_FIELD, event)
+    commit(types.SET_DICTIONARY_KEY, event)
   },
 
   deleteEvent({ commit }, event) {
     commit(types.DELETE_EVENT, event)
+    commit(types.UPDATE_DICTIONARY_KEY, event)
   },
 
   updateEventAddTransaction({ commit }, event) {
@@ -105,6 +112,44 @@ const mutations = {
       state.data.splice(i, 1)
     }
     state.transactions.push({ type: 'DELETE', event: event })
+  },
+
+  [types.BUILD_DICTIONARY] (state) {
+    const sortedEvents = _.sortBy(state.data, [ o => -moment(o.time).valueOf() ])
+    const dict = {}
+    _.each(sortedEvents, e => {
+      if (!dict[e.title]) {
+        dict[e.title] = {
+          id: e.id,
+          time: e.time
+        }
+      }
+    })
+    state.dictionary = dict
+  },
+
+  [types.SET_DICTIONARY_KEY] (state, event) {
+    if (!state.dictionary[event.title] || moment(state.dictionary[event.title].time) < moment(event.time)) {
+      state.dictionary[event.title] = {
+        id: event.id,
+        time: event.time
+      }
+    }
+  },
+
+  [types.UPDATE_DICTIONARY_KEY] (state, event) {
+    if (state.dictionary[event.title] && state.dictionary[event.title].id !== event.id) {
+      return
+    }
+    const latest = _.maxBy(_.filter(state.data, e => e.title === event.title), [ o => moment(o.time).valueOf()])
+    if (latest) {
+      state.dictionary[latest.title] = {
+        id: latest.id,
+        time: latest.time
+      }
+    } else {
+      delete state.dictionary[event.title]
+    }
   }
 }
 
