560569b91d952532e666bea039e595a75cf51721
Guy Brukhis <guy.br7@gmail.com>
2017-02-21 06:01:04 +0200

    api calls - add 'with-credentials' header to accept cross-domain cookies

--- src/api/API.js
+++ src/api/API.js
@@ -5,36 +5,43 @@ const API_BASE_URL = process.env.NODE_ENV === 'production' ? '//' : 'http://loca
 export default {
   get(url, params) {
     return new Promise((resolve, reject) => {
-      axios.get(API_BASE_URL + url)
-        .then(response => {
-          if (response.data.success === true) {
-            resolve(response.data)
-          } else if (response.data) {
-            reject(response.data)
-          } else {
-            reject(response)
-          }
-        })
-        .catch(error => {
-          reject(error)
-        })
+      axios({
+        method: 'get',
+        url: API_BASE_URL + url,
+        withCredentials: true
+      }).then(response => {
+        if (response.data.success === true) {
+          resolve(response.data)
+        } else if (response.data) {
+          reject(response.data)
+        } else {
+          reject(response)
+        }
+      })
+      .catch(error => {
+        reject(error)
+      })
     })
   },
   post(url, params) {
     return new Promise((resolve, reject) => {
-      axios.post(API_BASE_URL + url, params)
-        .then(response => {
-          if (response.data.success === true) {
-            resolve(response.data)
-          } else if (response.data) {
-            reject(response.data)
-          } else {
-            reject(response)
-          }
-        })
-        .catch(error => {
-          reject(error)
-        })
+      axios({
+        method: 'post',
+        url: API_BASE_URL + url,
+        data: params,
+        withCredentials: true
+      }).then(response => {
+        if (response.data.success === true) {
+          resolve(response.data)
+        } else if (response.data) {
+          reject(response.data)
+        } else {
+          reject(response)
+        }
+      })
+      .catch(error => {
+        reject(error)
+      })
     })
   }
 }
