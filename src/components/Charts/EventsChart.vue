0a282d762de2bc0109fc0c341aec14f400abfab2
Guy Brukhis <guy.br7@gmail.com>
2017-02-25 17:56:20 +0200

    WIP dashboard line chart

---
+++ src/components/Charts/EventsChart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="">
+    <line-chart v-if="options.type === 'line'" :chartData="eventsData"></line-chart>
+    <div v-else> Unknown {{ options.type }}</div>
+  </div>
+</template>
+
+<script>
+import LineChart from './LineChart'
+
+export default {
+  props: ['events', 'options'],
+  components: { LineChart },
+  computed: {
+    eventsData() {
+      return {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [
+          {
+            label: 'Label label',
+            backgroundColor: '#f87979',
+            data: [10, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+          }
+        ]
+      }
+    }
+  },
+  data() {
+    return {
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    Dashboard
+  <div class="dashboard">
+    <div>Dashboard</div>
+    <events-chart :events="events" :options="options"></events-chart>
   </div>
 </template>
 
 <script>
+import EventsChart from './Charts/EventsChart'
+export default {
+  components: { EventsChart },
+  computed: {
+    events(state) {
+      return state.$store.getters.events.data
+    },
+    options() {
+      return {
+        type: 'line', // line/bar/histogram
+        group_by: 'day', // hour/day/week/month
+        group_value: 'sum', // sum/average/median
+        datasets: [
+          {
+            label: 'Coffee',
+            event_match: 'coffee'
+          }
+        ]
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
+.dashboard {
+  height: 100%;
+  overflow-y: scroll;
+}
 </style>
