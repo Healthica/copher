99d6627a1c03ba258bd57ecf64726066c648371f
Guy Brukhis <guy.br7@gmail.com>
2017-03-26 13:30:24 +0300

    add event templates for popular events + add coffee template

--- src/components/Events/NewEventParser.js
+++ src/components/Events/NewEventParser.js
@@ -1,8 +1,62 @@
 // import nlp from 'compromise'
-import moment from 'moment'
+
+const predefined_events = {
+  coffee: {
+    fields: [{
+      title: 'Type',
+      type: 'select',
+      value: 'Espresso',
+      options: {
+        options: [
+          'Espresso',
+          'Instant',
+          'Capsules',
+          'Drip/Filtered',
+          'French Press',
+          'Turkish'
+        ]
+      }
+    }, {
+      title: 'Milk',
+      type: 'select',
+      value: 'None',
+      options: {
+        options: [
+          'None',
+          'Cold',
+          'Skim',
+          'Soy'
+        ]
+      }
+    }, {
+      title: 'Size',
+      type: 'select',
+      value: 'Regular',
+      options: {
+        options: [
+          'Small',
+          'Regular',
+          'Large',
+          'Extra Large'
+        ]
+      }
+    }, {
+      title: 'Sugar',
+      type: 'weight',
+      value: 0,
+      options: {
+        units: 'Teaspoon'
+      }
+    }, {
+      title: 'Decaffeinated',
+      type: 'switch',
+      value: false
+    }]
+  }
+}
 
 export default (text) => {
-  const event = {}
+  let event = {}
   const fields = []
 
   // const r = nlp(text)
@@ -12,6 +66,10 @@ export default (text) => {
   // console.log('dates', r.dates().data())
   // console.log('values', r.values().data())
   // console.log('people', r.people().data())
+
+  if (typeof predefined_events[text.toLowerCase()] !== undefined) {
+    event = predefined_events[text.toLowerCase()]
+  }
 
   return event
 }
