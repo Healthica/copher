c2c1f2e8d535e6f6c2c6e8a5f25cfa9dda3bc864
Guy Brukhis <guy.br7@gmail.com>
2017-03-27 15:20:39 +0300

    goals - add speed-dial

--- src/components/Goals.vue
+++ src/components/Goals.vue
@@ -1,24 +1,63 @@
 <template>
-  <div>
+  <div class="goals">
     <h2>Goals</h2>
+    <speed-dial class="new-goal" :items="newGoalItems" @open="createGoal"></speed-dial>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import SpeedDial from './Utils/SpeedDial'
 
 export default {
+  components: {
+    SpeedDial
+  },
   computed: {
     ...mapGetters([])
   },
   methods: {
+    createGoal(goal) {
+      console.log('goal', goal)
+    }
   },
   data() {
     return {
+      newGoalItems: [
+        {
+          title: 'Add New Goal',
+          color: 'primary',
+          icon: 'plus',
+          data: 1
+        }, {
+          title: 'Weekly Goal',
+          color: 'red',
+          icon: 'plus',
+          data: 2
+        }, {
+          title: 'One-Time Goal',
+          color: 'green',
+          icon: 'plus',
+          data: 3
+        }
+      ]
     }
   }
 }
 </script>
 
 <style>
+.goals {
+  height: 100%;
+  overflow-y: scroll;
+  background-color: #EDEFF4;
+}
+.goals::-webkit-scrollbar {
+  display: none;
+}
+.new-goal {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
 </style>
