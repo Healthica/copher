24c4f260baf0e4af1402e59a6d94f9b3d563cece
Guy Brukhis <guy.br7@gmail.com>
2017-03-27 15:52:01 +0300

    WIP edit goal modal

--- src/components/Goals.vue
+++ src/components/Goals.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="goals">
     <h2>Goals</h2>
+    <el-dialog v-model="goalModalVisible" @close="closeGoalModal">
+      <goal-edit-modal :goal="goalCopy" @close="closeGoalModal"></goal-edit-modal>
+    </el-dialog>
     <speed-dial class="new-goal" :items="newGoalItems" @open="createGoal"></speed-dial>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import GoalEditModal from './Goals/GoalEditModal'
 import SpeedDial from './Utils/SpeedDial'
 
 export default {
   components: {
+    GoalEditModal,
     SpeedDial
   },
   computed: {
@@ -18,11 +23,41 @@ export default {
   },
   methods: {
     createGoal(goal) {
-      console.log('goal', goal)
+      //TODO create new goal
+      const id = 1234
+      this.showGoalModal(id)
+    },
+    closeGoalModal() {
+      this.goalModalVisible = false
+    },
+    showGoalModal(id) {
+      //TODO clone from store
+      this.goalCopy = _.cloneDeep({ id: id, title: 'Goal Name' })
+      this.openGoalModal()
+    },
+    openGoalModal() {
+      this.goalModalVisible = true
+      this.goalCopyUnwatcher = this.$watch('goalCopy', this.onChangeGoal, { deep: true })
+    },
+    closeGoalModal() {
+      this.eventModalVisible = false
+    },
+    closeGoalModal() {
+      this.goalCopyUnwatcher()
+      this.goalCopyUnwatcher = null
+      if (this.goalCopy._isDeleted !== true) {
+        this.$store.dispatch('updateEventAddTransaction', _.cloneDeep(this.goalCopy))
+      }
+    },
+    onChangeGoal(e) {
+      this.$store.dispatch('setGoal', e)
     }
   },
   data() {
     return {
+      goalModalVisible: false,
+      goalCopy: {},
+      goalCopyUnwatcher: null,
       newGoalItems: [
         {
           title: 'Add New Goal',
---
+++ src/components/Goals/GoalEditModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="modal">
+    <h2>Goal #{{ goal.id }}</h2>
+
+    <div class="close-modal-button">
+      <el-button @click="close" type="primary" size="small" :plain="true">Close</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['goal'],
+  data() {
+    return {
+      editMode: false
+    }
+  },
+  computed: {
+  },
+  methods: {
+    close() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
