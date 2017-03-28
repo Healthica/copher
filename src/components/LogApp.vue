d519e4013813b8bff8e49b7dd675ff1b3b479863
Guy Brukhis <guy.br7@gmail.com>
2017-03-28 19:48:03 +0300

    edit event modal - disable close on background click

--- src/components/LogApp.vue
+++ src/components/LogApp.vue
@@ -12,7 +12,7 @@
       </li>
       <event-row v-for="e in day.events" :event="e" :clickHandler="showEditEventModal" @duplicateEvent="duplicateEvent"></event-row>
     </ul>
-    <el-dialog custom-class="eventModal" v-model="eventModalVisible" @close="onCloseEditEventModal" size="large">
+    <el-dialog custom-class="eventModal" v-model="eventModalVisible" @close="onCloseEditEventModal" size="large" :close-on-click-modal="false">
       <event-edit-modal :event="eventCopy" @close="closeEditEventModal" @duplicateEvent="duplicateEvent" ref="EventEditModal"></event-edit-modal>
     </el-dialog>
     <onboarding-modal></onboarding-modal>
